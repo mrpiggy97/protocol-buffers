e3e1ffdee8daf9c726ec3a04d55e71196908e270
mrpiggy97 <fabyjesusrivas10@gmail.com>
2021-12-04 22:11:26 +0000

    made changes to protofiles and created a new one

--- user/User.pb.go
+++ user/User.pb.go
@@ -20,6 +20,100 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+type Developer struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Language string `protobuf:"bytes,1,opt,name=language,proto3" json:"language,omitempty"`
+}
+
+func (x *Developer) Reset() {
+	*x = Developer{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protofiles_User_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Developer) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Developer) ProtoMessage() {}
+
+func (x *Developer) ProtoReflect() protoreflect.Message {
+	mi := &file_protofiles_User_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Developer.ProtoReflect.Descriptor instead.
+func (*Developer) Descriptor() ([]byte, []int) {
+	return file_protofiles_User_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *Developer) GetLanguage() string {
+	if x != nil {
+		return x.Language
+	}
+	return ""
+}
+
+type Operator struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Platform string `protobuf:"bytes,1,opt,name=platform,proto3" json:"platform,omitempty"`
+}
+
+func (x *Operator) Reset() {
+	*x = Operator{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protofiles_User_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Operator) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Operator) ProtoMessage() {}
+
+func (x *Operator) ProtoReflect() protoreflect.Message {
+	mi := &file_protofiles_User_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Operator.ProtoReflect.Descriptor instead.
+func (*Operator) Descriptor() ([]byte, []int) {
+	return file_protofiles_User_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *Operator) GetPlatform() string {
+	if x != nil {
+		return x.Platform
+	}
+	return ""
+}
+
 type User struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -27,12 +121,16 @@ type User struct {
 
 	UserId string `protobuf:"bytes,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
 	Email  string `protobuf:"bytes,2,opt,name=email,proto3" json:"email,omitempty"`
+	// Types that are assignable to Type:
+	//	*User_Developer
+	//	*User_Operator
+	Type isUser_Type `protobuf_oneof:"type"`
 }
 
 func (x *User) Reset() {
 	*x = User{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_protofiles_User_proto_msgTypes[0]
+		mi := &file_protofiles_User_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -45,7 +143,7 @@ func (x *User) String() string {
 func (*User) ProtoMessage() {}
 
 func (x *User) ProtoReflect() protoreflect.Message {
-	mi := &file_protofiles_User_proto_msgTypes[0]
+	mi := &file_protofiles_User_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -58,7 +156,7 @@ func (x *User) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use User.ProtoReflect.Descriptor instead.
 func (*User) Descriptor() ([]byte, []int) {
-	return file_protofiles_User_proto_rawDescGZIP(), []int{0}
+	return file_protofiles_User_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *User) GetUserId() string {
@@ -75,16 +173,66 @@ func (x *User) GetEmail() string {
 	return ""
 }
 
+func (m *User) GetType() isUser_Type {
+	if m != nil {
+		return m.Type
+	}
+	return nil
+}
+
+func (x *User) GetDeveloper() *Developer {
+	if x, ok := x.GetType().(*User_Developer); ok {
+		return x.Developer
+	}
+	return nil
+}
+
+func (x *User) GetOperator() *Operator {
+	if x, ok := x.GetType().(*User_Operator); ok {
+		return x.Operator
+	}
+	return nil
+}
+
+type isUser_Type interface {
+	isUser_Type()
+}
+
+type User_Developer struct {
+	Developer *Developer `protobuf:"bytes,3,opt,name=developer,proto3,oneof"`
+}
+
+type User_Operator struct {
+	Operator *Operator `protobuf:"bytes,4,opt,name=operator,proto3,oneof"`
+}
+
+func (*User_Developer) isUser_Type() {}
+
+func (*User_Operator) isUser_Type() {}
+
 var File_protofiles_User_proto protoreflect.FileDescriptor
 
 var file_protofiles_User_proto_rawDesc = []byte{
 	0x0a, 0x15, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x73, 0x2f, 0x55, 0x73, 0x65,
 	0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0a, 0x75, 0x73, 0x65, 0x72, 0x45, 0x6e, 0x74,
-	0x69, 0x74, 0x79, 0x22, 0x35, 0x0a, 0x04, 0x55, 0x73, 0x65, 0x72, 0x12, 0x17, 0x0a, 0x07, 0x75,
-	0x73, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x75, 0x73,
-	0x65, 0x72, 0x49, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x02, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x42, 0x08, 0x5a, 0x06, 0x2e, 0x2f,
-	0x75, 0x73, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x69, 0x74, 0x79, 0x22, 0x27, 0x0a, 0x09, 0x44, 0x65, 0x76, 0x65, 0x6c, 0x6f, 0x70, 0x65, 0x72,
+	0x12, 0x1a, 0x0a, 0x08, 0x6c, 0x61, 0x6e, 0x67, 0x75, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x08, 0x6c, 0x61, 0x6e, 0x67, 0x75, 0x61, 0x67, 0x65, 0x22, 0x26, 0x0a, 0x08,
+	0x4f, 0x70, 0x65, 0x72, 0x61, 0x74, 0x6f, 0x72, 0x12, 0x1a, 0x0a, 0x08, 0x70, 0x6c, 0x61, 0x74,
+	0x66, 0x6f, 0x72, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x70, 0x6c, 0x61, 0x74,
+	0x66, 0x6f, 0x72, 0x6d, 0x22, 0xa8, 0x01, 0x0a, 0x04, 0x55, 0x73, 0x65, 0x72, 0x12, 0x17, 0x0a,
+	0x07, 0x75, 0x73, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06,
+	0x75, 0x73, 0x65, 0x72, 0x49, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x12, 0x35, 0x0a, 0x09,
+	0x64, 0x65, 0x76, 0x65, 0x6c, 0x6f, 0x70, 0x65, 0x72, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32,
+	0x15, 0x2e, 0x75, 0x73, 0x65, 0x72, 0x45, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x2e, 0x44, 0x65, 0x76,
+	0x65, 0x6c, 0x6f, 0x70, 0x65, 0x72, 0x48, 0x00, 0x52, 0x09, 0x64, 0x65, 0x76, 0x65, 0x6c, 0x6f,
+	0x70, 0x65, 0x72, 0x12, 0x32, 0x0a, 0x08, 0x6f, 0x70, 0x65, 0x72, 0x61, 0x74, 0x6f, 0x72, 0x18,
+	0x04, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x75, 0x73, 0x65, 0x72, 0x45, 0x6e, 0x74, 0x69,
+	0x74, 0x79, 0x2e, 0x4f, 0x70, 0x65, 0x72, 0x61, 0x74, 0x6f, 0x72, 0x48, 0x00, 0x52, 0x08, 0x6f,
+	0x70, 0x65, 0x72, 0x61, 0x74, 0x6f, 0x72, 0x42, 0x06, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x42,
+	0x08, 0x5a, 0x06, 0x2e, 0x2f, 0x75, 0x73, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
@@ -99,16 +247,20 @@ func file_protofiles_User_proto_rawDescGZIP() []byte {
 	return file_protofiles_User_proto_rawDescData
 }
 
-var file_protofiles_User_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_protofiles_User_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_protofiles_User_proto_goTypes = []interface{}{
-	(*User)(nil), // 0: userEntity.User
+	(*Developer)(nil), // 0: userEntity.Developer
+	(*Operator)(nil),  // 1: userEntity.Operator
+	(*User)(nil),      // 2: userEntity.User
 }
 var file_protofiles_User_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: userEntity.User.developer:type_name -> userEntity.Developer
+	1, // 1: userEntity.User.operator:type_name -> userEntity.Operator
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_protofiles_User_proto_init() }
@@ -118,6 +270,30 @@ func file_protofiles_User_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_protofiles_User_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Developer); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_protofiles_User_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Operator); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_protofiles_User_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*User); i {
 			case 0:
 				return &v.state
@@ -130,13 +306,17 @@ func file_protofiles_User_proto_init() {
 			}
 		}
 	}
+	file_protofiles_User_proto_msgTypes[2].OneofWrappers = []interface{}{
+		(*User_Developer)(nil),
+		(*User_Operator)(nil),
+	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protofiles_User_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
